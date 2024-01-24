481ca30235be47f4aa4068c89d418b47d03b6858
Michał Knapiński <mkknapinski@gmail.com>
2024-01-24 22:52:06 +0100

    Added automatic .env file handling. Added django command to wait for the database to be available and set up.

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +81,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': env('DB_HOST'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASS')
     }
 }
 
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,46 @@
+"""
+Test custom Django management commands.
+"""
+
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands"""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready."""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    # the order or patched arguments is important
+    # it applies the arguments from the inside out
+    # first we've got self, which is always the first
+    # then patched_sleep because we mock method on method level
+    # then patched_check because it's mocked at class level
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError."""
+        # the first 2 times we call the method we want to raise a Psycopg2Error
+        # \ breaks into multiple lines
+        # next 3 times we raise OperationalError
+        # finally we get True back. It's not an exception, so it knows to return it
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+                                    [OperationalError] * 3 + \
+                                    [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        self.assertEqual(patched_sleep.call_count, 5)
+        patched_check.assert_called_with(databases=['default'])
